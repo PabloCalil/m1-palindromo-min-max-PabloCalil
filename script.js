2ad24b41502a6a1318d916fa8ec7da8b5ffba915
Pablo Calil <pablocalilsousa@gmail.com>
2024-06-06 13:16:06 -0300

    PabloCalil
    Realização das funções e dos loops.

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    str = str.toLowerCase().replaceAll(' ', ''); 
+    let i = 0;
+    let j = str.length -1;
+
+    while(i < j) {
+        if(str[i] !== str[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+
+    return true; 
 }
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let minValor = 0;
+    let maxValor = 0;
+
+    for(let i of arr) {
+        if(minValor === 0 && maxValor === 0) {
+            minValor = i;
+            maxValor = i;
+        }
+        if(i < minValor) {
+            minValor = i;
+        } else if(i > maxValor) {
+            maxValor = i;
+        }
+    }
+
+   return [minValor, maxValor];
 }
